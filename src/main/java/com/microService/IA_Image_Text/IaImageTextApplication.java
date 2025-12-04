0e360e11e099f88889a86514172a97b60fb26a04
SebitasDown <areizasebas31@gmail.com>
2025-12-04 09:02:45 -0500

    basic folders of created microservice

---
+++ src/main/java/com/microService/IA_Image_Text/IaImageTextApplication.java
@@ -0,0 +1,13 @@
+package com.microService.IA_Image_Text;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IaImageTextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IaImageTextApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microService/IA_Image_Text/IaImageTextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microService.IA_Image_Text;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IaImageTextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
