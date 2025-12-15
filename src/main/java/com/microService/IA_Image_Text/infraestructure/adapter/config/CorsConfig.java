41ecd03e3660c444e003355853954973ff584868
SebitasDown <areizasebas31@gmail.com>
2025-12-15 14:15:13 -0500

    fix cors

--- src/main/java/com/microService/IA_Image_Text/infraestructure/adapter/config/CorsConfig.java
+++ src/main/java/com/microService/IA_Image_Text/infraestructure/adapter/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/v1/**")
+        registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000", "https://tu-frontend-production.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
